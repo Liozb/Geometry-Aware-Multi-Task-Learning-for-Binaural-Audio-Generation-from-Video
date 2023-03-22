8a83f47c763ef535e8735c20fc8e8b491e77557d
Liozb <liozberman@gmail.com>
2023-03-22 19:23:00 +0200

    called resnet18 model on the frames

--- backBone.py
+++ backBone.py
@@ -4,21 +4,34 @@ from torch.utils.data import Dataset, DataLoader
 import torchaudio
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
+import torchvision.models as models
 
+resnet18 = models.resnet18(pretrained=True)
+
+
+frames_path = "/dsi/gannot-lab/datasets2/FAIR-Play/frames_30fps/"
+batch_size = 64
 
 transform = transforms.Compose([
-    transforms.Resize((224, 224)),
+    transforms.Resize(224),
+    transforms.CenterCrop(224),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
-dataset = datasets.ImageFolder(root='/dsi/gannot-lab/datasets2/FAIR-Play/frames_30fps/', transform=transform)
+dataset = datasets.ImageFolder(root=frames_path, transform=transform)
+loader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=False)
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+resnet18.eval()
+resnet18.to(device)
 
-dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
-for images, labels in dataloader:
-    print(images)
-    # Do something with the batch of images and labels
+with torch.no_grad():
+    for images, _ in loader:
+        images = images.to(device)
+        outputs = resnet18(images)
+        _, predicted = torch.max(outputs, 1)
+        print(predicted)
 
 class FairPlayAudio(Dataset):
     def __init__(self, audio, labels):
