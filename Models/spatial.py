b1bac7e6f107ed7b01de5f1005a9ade30649fd1e
tamar <tamar.mech@gmail.com>
2023-05-31 19:04:35 +0300

    save changes

--- Models/spatial.py
+++
@@ -1,9 +0,0 @@
-# this file responsible for the spatial coherence train model
-
-import sys
-import os
-from Models.backbone_model import *
-DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.dirname(DIR))
-from imports import *
-from Datasets.AudioVisualDataset import AudioVisualDataset
